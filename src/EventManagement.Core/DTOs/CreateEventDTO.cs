d7599a527db19c0f74f0b24c8598b9c21b516754
Permafrost Wolf <perma@howlpoint.com>
2025-02-14 22:01:53 -0700

    BROKEN- DBCONJTEXT
    Dbcontext broken after converting from monolithic to microservice architecture. Pushing for a break point will fix later. The project will expand on further concepts to make this cleaner and run in the cloud better.,

---
+++ src/EventManagement.Core/DTOs/CreateEventDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagement.Core.DTOs;
+
+public class CreateEventDto
+{
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ src/EventManagement.Services.Events/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using EventManagement.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagement.Services.Events.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Event> Events { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.ToTable("events", "event_management");
+            entity.HasKey(e => e.Id);
+            
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            entity.Property(e => e.IsDraft)
+                .HasDefaultValue(true);
+        });
+    }
+}
