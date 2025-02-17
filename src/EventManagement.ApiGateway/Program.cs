852ee926ae5dda364b0b7e8c389e0875675036db
Permafrost Wolf <perma@howlpoint.com>
2025-02-17 10:48:27 -0700

    Un-fu*ked Program.cs

---
+++ src/EventManagement.ApiGateway/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Comment out HTTPS redirection for now
+//app.UseHttpsRedirection();
+
+// Map reverse proxy routes
+app.MapReverseProxy();
+
+app.Run();
